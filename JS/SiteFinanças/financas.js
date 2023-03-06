815a91e97fd32eb21d49543d04df6d86943f82fa
Vanessa <vane.schlemper@gmail.com>
2023-03-06 10:08:03 -0300

    site finanças

--- JS/SiteFinanças/financas.js
+++ JS/SiteFinanças/financas.js
@@ -1,96 +1,170 @@
-let tbody = document.querySelector("tbody");
-let descItem = document.querySelector("#desc");
-let amount = document.querySelector("#amount");
-let type = document.querySelector("#type");
-let btnNew = document.querySelector("#btnNew");
+// let form  = document.getElementById('transaction-form');
+// let submitButton = document.getElementById('transaction-submit');
 
-let incomes = document.querySelector(".entrada");
-let expenses = document.querySelector(".saida");
-let total = document.querySelector(".total");
+// submitButton.addEventListener('click', function(event){
+//     event.preventDefault();
 
-let items;
+// let amountInput = document.getElementById('amount-input');
+// let typeSelect = document.getElementById('type-select');
+// let amount = Number(amountInput.value);
+// let type = typeSelect.value;
 
-btnNew.onclick = function () {
-    if(descItem.value === "" || amount.value === "" || type.value === ""){
-        return alert("Preencha todas as informações!")
+// if(isNaN(amount)){
+//     alert('Por favor, informe um valor válido.');
+//     return;
+// }
+
+// let transaction = {
+//     amount: amount,
+//     type: type
+// };
+
+// let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+// transactions.push(transaction);
+// localStorage.setItem('transactions', JSON.stringify(transactions));
+
+// updateTransactionHistory();
+// });
+
+// function updateTransactionHistory(){
+//     let tableBody = document.querySelector('#transaction-history tbody');
+//     tableBody.innerHTML = '';
+
+//     let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
+// for (let transaction of transactions){
+//     let row = document.createElement('tr');
+//     let amountCell = document.createElement('td');
+//     let typeCell = document.createElement('td');
+
+//     amountCell.textContent = transaction.amount;
+//     typeCell.textContent = transaction.type;
+    
+//     row.appendChild(amountCell);
+//     row.appendChild(typeCell);
+
+//     tableBody.appendChild(row);
+    
+// } 
+// }
+
+// updateTransactionHistory();
+
+
+
+
+
+// Criação de variáveis globais
+let transactionHistory = [];
+
+const amountInput = document.getElementById("amount-input");
+const typeSelect = document.getElementById("type-select");
+const transactionForm = document.getElementById("transaction-form");
+const transactionTableBody = document.querySelector("#transaction-history tbody");
+const totalCard = document.querySelector(".card3 p.total");
+const incomeCard = document.querySelector(".card1 p.entrada");
+const expenseCard = document.querySelector(".card2 p.saida");
+
+// Função para salvar a transação no localStorage
+function saveTransactionToLocalStorage(transaction) {
+  let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+  transactions.push(transaction);
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+// Função para excluir a transação do localStorage
+function deleteTransactionFromLocalStorage(index) {
+  let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+  transactions.splice(index, 1);
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+// Função para atualizar a tabela de histórico de transações
+function updateTransactionTable(transaction) {
+  const tr = document.createElement("tr");
+
+  tr.innerHTML = `
+        <div class="botaozinho">
+        <td class="classTD">${transaction.amount}</td>
+        <td class="classTD">${transaction.type}</td>
+        <td><button class="delete-btn">Excluir</button></td>
+        </div>
+    `;
+
+  transactionTableBody.appendChild(tr);
+}
+
+// Função para atualizar os cards com os totais
+function updateTotalCard() {
+  let total = 0;
+  let income = 0;
+  let expense = 0;
+
+  transactionHistory.forEach((transaction) => {
+    if (transaction.type === "Entrada") {
+      income += transaction.amount;
+      total += transaction.amount;
+    } else {
+      expense += transaction.amount;
+      total -= transaction.amount;
     }
+  });
 
-    items.push({
-        desc: descItem.value,
-        amount: Math.abs(amount.value).toFixed(2),
-        type: type.value,
+  totalCard.textContent = `R$ ${total.toFixed(2)}`;
+  incomeCard.textContent = `R$ ${income.toFixed(2)}`;
+  expenseCard.textContent = `R$ ${expense.toFixed(2)}`;
+}
+
+// Função para carregar as transações do localStorage
+function loadTransactions() {
+  let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+  transactions.forEach((transaction) => {
+    transactionHistory.push(transaction);
+    updateTransactionTable(transaction);
+  });
+
+  updateTotalCard();
+}
+
+// Evento para excluir a transação da tabela e do localStorage
+transactionTableBody.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-btn")) {
+    const index = Array.from(transactionTableBody.children).indexOf(
+      event.target.parentElement.parentElement
+    );
+
+    deleteTransactionFromLocalStorage(index);
+    transactionHistory.splice(index, 1);
+    event.target.parentElement.parentElement.remove();
+    updateTotalCard();
+  }
 });
 
-    setItensDB();
-    loadItens();
+// Evento para adicionar a transação na tabela e no localStorage
+transactionForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    descItem.value = "";
-    amount.value = "";
-    
-}
+  const transaction = {
+    amount: parseFloat(amountInput.value),
+    type: typeSelect.value,
+  };
 
-function deleteItem(item){
-    items.splice(index,1);
-    setItensDB();
-    loadItens();
-}
+  transactionHistory.push(transaction);
+  saveTransactionToLocalStorage(transaction);
+  updateTransactionTable(transaction);
+  updateTotalCard();
 
-function insertItem(item,index) {
-    let ol = document.createElement("ol");
+  amountInput.value = "";
+});
 
-    ol.innerHTML =`
-        <li>${item.desc}</li>
-        <li>R$ ${item.amount}</li>
-        <li class="columnType">${
-            item.type === "entrada"
-            ?'<i class = "bx bxs-chevron-up-circle"></i>'
-            :'<i class = "bx bxs-chevron-down-circle"></i>'
-        } </li>
-        <li class ="columnAction">
-            <button onclick= "deleteItem(${index})"><i class="bx bx-trash"></button>
-        </li>
-    `;
-    tbody.appendChild(ol)
-}
+// Chamada da função para carregar as transações do localStorage
+loadTransactions();
 
-function loadItens(){
-    items = getItensDB();
-    tbody.innerHTML = "";
-    items.forEach((item,index) => {
-        insertItem(item,index);
-    });
-    getTotals();
-}
 
-function getTotals(){
-    let amountIncomes = items
-    .filter((item) => item.type === "entrada")
-    .map((transaction) => Number(transaction.amount));
 
-    let amountExpenses = items
-    .filter((item) => item.type === "saida")
-    .map((transaction) => Number(transaction.amount));
 
-    let totalIncomes = amountIncomes
-        .reduce((acc, cur) => acc + cur, 0)
-        .tofixed(2);
-
-    let totalExpenses = Math.abs(amountExpenses.reduce((acc, cur) => acc + cur, 0))
-    .toFixed(2);
-
-    let totalItems = (totalIncomes - totalExpenses).toFixed(2);
-    
-    incomes.innerHTML = totalIncomes;
-    incomes.innerHTML = totalExpenses;
-    total.innerHTML = totalItems;
-
-}
-
-let getItensDB = () => JSON.parse(localStorage.getItem("db_items")) ?? [];
-
-let setItensDB = () => localStorage.setItem("db_items", JSOn.stringify(items));
-
-loadItens();
+  
 
 
 
