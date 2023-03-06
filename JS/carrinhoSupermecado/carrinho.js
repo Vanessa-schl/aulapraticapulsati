cea01780a4f7f158889e9b0986f360af6d327b7c
Vanessa <vane.schlemper@gmail.com>
2023-03-06 17:16:45 -0300

    carinho de supermecado

---
+++ JS/carrinhoSupermecado/carrinho.js
@@ -0,0 +1,17 @@
+const cartButton = document.getElementById("cart-button");
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".close");
+
+cartButton.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
